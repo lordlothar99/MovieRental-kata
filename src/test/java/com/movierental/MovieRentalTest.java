509aa2f709be05138c7f581e3f1a34a0bd8e2f05
François Lecomte <francois.lecomte-ext9sgcib.com>
2015-05-06 16:39:27 +0200

    remove tests

--- src/test/java/com/movierental/MovieRentalTest.java
+++ src/test/java/com/movierental/MovieRentalTest.java
@@ -1,26 +1,6 @@
 package com.movierental;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Test;
-
-import com.movierental.Customer;
-import com.movierental.Movie;
-import com.movierental.Rental;
 
 public class MovieRentalTest {
 
-	@Test
-	public void testStatement() {
-		Movie movie = new Movie("Transformer", Movie.REGULAR);
-
-		Rental rental = new Rental(movie, 3);
-
-		Customer customer = new Customer("Alice");
-		customer.addRental(rental);
-
-		String statement = customer.statement();
-		assertThat(statement, containsString("Transformer"));
-	}
 }
